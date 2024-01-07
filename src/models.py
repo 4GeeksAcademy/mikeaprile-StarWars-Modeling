b258fbd3a1d63b3acacd468ecf23759b617d6aa6
Michael Aprile Mancha <130480625+mikeaprilee@users.noreply.github.com>
2024-01-07 10:40:10 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -1,32 +1,58 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String(20), nullable=False)
+    last_name = Column(String(20), nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String(15), nullable=False)
+    suscription_date = Column(Date)
+    is_login = Column(Boolean, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String, nullable=False)
+    climate = Column(String)
+    diameter = Column(Integer)
+    orbital_period = Column(Integer)
+    population = Column(Integer)
+    rotation_period = Column(Integer)   
+    terrain = Column(String)
 
-    def to_dict(self):
-        return {}
+class FavouritePlanets(Base):
+    __tablename__ = 'favourite_planets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id')) # Defino clave foránea
+    user = relationship(Users) # Defino la relacion que tiene la clave foránea
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet = relationship(Planets)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    birth_year = Column(Date)
+    eye_color = Column(String)
+    gender = Column(String, nullable=False)
+    mass = Column(Integer) 
+    skin_color = Column(String)
+
+class FavouriteCharacters(Base):
+    __tablename__ = 'favourite_characters'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(Users)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship(Characters)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
